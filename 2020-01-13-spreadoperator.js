b7a82b35860f86296e4aa5c35e7c083769d2b1d8
jbdevelop <jbernardorj@gmail.com>
2020-01-13 23:11:31 -0300

    spreadoperator

---
+++ 2020-01-13-spreadoperator.js
@@ -0,0 +1,11 @@
+<script>
+
+let animals = ['Monkey', 'Lion', 'Elephant']
+
+let moreAnimals = ['Zebra', 'Lemur', 'Cheetah']
+
+let allAnimals = ['Hyena', ...animals, ...moreAnimals]
+
+console.log(allAnimals)
+
+</script>
