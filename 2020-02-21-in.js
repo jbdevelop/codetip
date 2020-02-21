f1a5d9ff2e2f123d92978f19f466c99b68193e03
jbdevelop <jbernardorj@gmail.com>
2020-02-21 14:59:24 -0300

    Codetip

--- 2020-02-21-in.js
+++ 2020-02-21-in.js
@@ -4,7 +4,7 @@ const sponsors = ['Nike', 'ELetrobrás', 'Coca-Cola']
 
 console.log('slogan' in sponsor) // true
 
-delete sponsor.slogan
+delete sponsor.slogan 
 
 console.log('slogan' in sponsor) // false
 
