6ce8de2f37985c96f1cd612d5efc8a0296b7701c
jbdevelop <jbernardorj@gmail.com>
2020-02-23 09:27:45 -0300

    Codetip

--- 2020-02-23-every.js
+++ 2020-02-23-every.js
@@ -1,3 +1,10 @@
+/*
+O método 𝘦𝘷𝘦𝘳𝘺 testa cada elemento do vetor a partir de uma 𝘤𝘢𝘭𝘭𝘣𝘢𝘤𝘬 e retorna um 
+𝘣𝘰𝘰𝘭𝘦𝘢𝘯. Se um desses testes for falso, 𝘦𝘷𝘦𝘳𝘺 vai parar, não verificará os valores 
+restantes do vetor e vai retornar 𝘧𝘢𝘭𝘴𝘦.
+Este exemplo simples verifica se todos os números são divisíveis por 2.
+*/
+
 const numbers = [2, 10, 22, 76, 8, 16]
 
 const divisibleBy = numbers.every( value => value % 2 === 0 )
