a8e8516f3839be35a0c52596b65524984be01a55
jbdevelop <jbernardorj@gmail.com>
2020-02-24 13:36:27 -0300

    Codetip

--- 2020-02-24-RegExp.js
+++ 2020-02-24-RegExp.js
@@ -1,3 +1,13 @@
+/*
+Uma 𝘙𝘦𝘨𝘶𝘭𝘢𝘳 𝘌𝘹𝘱𝘳𝘦𝘴𝘴𝘪𝘰𝘯 (𝘙𝘦𝘨𝘦𝘹) ou Expressão Regular é formada por uma sequência de 
+caracteres para se obter um padrão de busca. Podemos utilizar esse padrão para
+pesquisá-lo numa determinada 𝘴𝘵𝘳𝘪𝘯𝘨 e se necessário, fazer uma manipulação de 
+texto. Expressões regulares em conjunto com os métodos previstos se tornam 
+extremamente poderosos para manipulação de 𝘴𝘵𝘳𝘪𝘯𝘨𝘴. Os exemplos são simples e 
+incentivadores para um estudo mais aprofundado da documentação, de modo que se 
+aproveite o melhor dessa ferramenta. 
+*/
+
 //1a FORMA - através de Expressões Literais
 const literal = /node/ig.test(
    'Backend é com Node.js, Frontend é com React, Mobile é com React Native.'
