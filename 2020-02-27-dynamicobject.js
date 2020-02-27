a4c50520d617eab33b7e4425a9f78c776b7413d1
jbdevelop <jbernardorj@gmail.com>
2020-02-27 10:49:04 -0300

    Codetip

---
+++ 2020-02-27-dynamicobject.js
@@ -0,0 +1,12 @@
+const dynamic = 'kills'
+
+const character = {
+   name: 'John Wick',
+   [dynamic]: 0,
+}
+
+console.log(character) // { name: 'John Wick', kills: 0 }
+
+character.kills = 299
+
+console.log(character) // { name: 'John Wick', kills: 299 }
