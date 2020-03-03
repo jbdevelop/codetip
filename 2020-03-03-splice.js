e9a2e1e6071dc7372240a9b0543dc1525bf7a4fb
jbdevelop <jbernardorj@gmail.com>
2020-03-03 11:16:10 -0300

    Codetip

--- 2020-03-03-splice.js
+++ 2020-03-03-splice.js
@@ -1,3 +1,15 @@
+/*
+O método splice() permite inserir e/ou remover elementos no vetor a partir do 
+índice dado. Age sobre o vetor original e retorna os elementos excluídos.
+Parâmetros:
+𝘀𝘁𝗮𝗿𝘁 - Em qual ou a partir de qual índice deverá agir.
+𝗱𝗲𝗹𝗲𝘁𝗲𝗖𝗼𝘂𝗻𝘁 - número inteiro que indica quantos elementos devem ser excluídos. 
+Se omitido ou se o número indicado for maior que a quantidade de elementos 
+existentes no vetor, então todos os elementos a partir de 𝘀𝘁𝗮𝗿𝘁 serão removidos.
+𝗶𝘁𝗲𝗺𝟭, 𝗶𝘁𝗲𝗺𝟮, ... - Elementos a serem adicionados na lista. Se omitido, 
+splice() apenas removerá elementos.
+*/
+
 const numbers = [1, 7]
 
 //A partir do índice 1, insere 2,3,3,5,6 no vetor. Remove 0 elementos.  
