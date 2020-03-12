19cfd8c96e1058aa8d9d3ee57fe631ab63b78f1b
jbdevelop <jbernardorj@gmail.com>
2020-03-11 22:49:15 -0300

    Codetip

---
+++ 2020-03-11-operator===.js
@@ -0,0 +1,13 @@
+/* 
+O operador de identidade (===) compara os operandos sem fazer conversão de tipo 
+do dado. Já o operador de igualdade (==) converte o operando se ele não for 
+do mesmo tipo do outro e só depois faz a comparação estrita.
+*/
+
+console.log('3' == 3) // true
+
+console.log('7' == 7) // true
+
+console.log(10 === 10) // true
+
+console.log('5' === 5) // false
