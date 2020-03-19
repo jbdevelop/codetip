eccee4986893a0e45cc5aafccb4dccb0efa220cd
jbdevelop <jbernardorj@gmail.com>
2020-03-19 09:11:07 -0300

    Codetip

---
+++ 2020-03-19-trycatch.js
@@ -0,0 +1,40 @@
+// Vamos começar com um try...catch usual:
+try {
+   throw Error('Crash!') // Exceção lançada.
+} catch(error) { // Exceção capturada.
+   console.log(error)
+} finally { // Independente da exceção ter sido capturada ou lançada, finally vai executar.
+   console.log('I got it')
+}
+// Error: Crash
+// I got it
+
+// Agora, vamos aninhar sem o catch interno:
+try {
+   try {
+      throw Error('Boom!') 
+   } finally {
+      console.log('The End.')
+   }
+} catch(error) {
+   console.log(error)
+}
+// The End.
+// Error: Boom!
+
+// Por fim, vamos inserir o catch interno lançando novamente uma exceção:
+try {
+   try {
+      throw Error('Bang!') 
+   } catch (error) {
+      console.log(error)
+      throw error // *** Lançando novamente a exceção 'Bang!'.
+   } finally {
+      console.log('Goodbye.')
+   }
+} catch(error) { // *** Que é capturada neste catch.
+   console.log(error)
+}
+// Error: Bang!
+// Goodbye.
+// Error: Bang!
