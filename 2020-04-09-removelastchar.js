c1c1722edc30789cc089dc573ea3b165087f8784
jbdevelop <jbernardorj@gmail.com>
2020-04-09 16:57:14 -0300

    Codetip

---
+++ 2020-04-09-removelastchar.js
@@ -0,0 +1,4 @@
+const backEnd = "'node.js','python','php',"
+// substring vai capturar os caracteres do índice 0 até o penúltimo índice da string 
+const lastRemoved = backEnd.substring(0, backEnd.length-1);
+console.log(lastRemoved); //'node.js','python','php'
