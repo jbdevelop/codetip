09af7cca57a017dad01f31705d79c383839c9140
Julio Bernardo <jbernardo@gmail.com>
2020-12-27 15:56:38 -0300

    Compare Objects

---
+++ 2020-12-27-compareobjects.js
@@ -0,0 +1,27 @@
+const obj1 = [
+  {
+    id: 1,
+    name: "John",
+  },
+  {
+    id: 2,
+    name: "Martin",
+  },
+];
+
+const obj2 = [
+  {
+    id: 1,
+    name: "John",
+  },
+  {
+    id: 2,
+    name: "Martin",
+  },
+];
+
+console.log(JSON.stringify(obj1) === JSON.stringify(obj2)); // true
+
+obj2[0].name = "Frank";
+
+console.log(JSON.stringify(obj1) === JSON.stringify(obj2)); // false
